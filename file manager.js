2ee108a8b7aad664266a8ed1f9fbb00626b8d996
harik6036 <harik8050@gmail.com>
2022-06-08 16:39:59 +0530

    project

--- file manager.js
+++ file manager.js
@@ -1,41 +1,137 @@
-//organize
-//tree
-//help
-//
-//command dirpath
 
-let command=process.argv[2];
-let dirpath= process.argv[3];
-let treeobject=require("./tree.js");
-//let helpobject=require("../help");
-let organizeobject=require("./organize.js");
+const fs=require("fs");
+const path=require("path");
+const { cwd } = require("process");
+let types={
+audio:["mpeg","mp3"],
+video:["avi","mp4","mkv"],
+documents:["docs","pdf","doc","xlsx"],
+image:["jpg","png","gif","jpeg"],
+app:["dmg","deb"],
+software:["exe"]
 
-// console.log(command);
-// console.log(dirpath);
-switch(command)
-{
-    case "organize":
-        organizeobject.organizekey(dirpath);
-       break;
-
-      
-    case "help":
-        //console.log("the help fn is implemented");
-        helpfn()
-        break;
-
-        default:
-            console.log("enter the correct command");
 }
 
 
+let inputarr=process.argv.slice(2);
+let command=inputarr[0]
+let dirpath=inputarr[1];
 
+//console.log(command);
+switch(command){
+case "organize":
+    organizeFn(dirpath);
+    break;
 
-function helpfn(){
-console.log(`following are the commands
-1 organize "dirpath"
-2 tree "dirpath"
-3 help`);
+    case "tree":
+        treeFn(dirpath);
+
+        break;
+        case "help":
+        helpfn();
+
+}
+function organizeFn(dirPath) {
+    // console.log("organize command implemnted for ", dirPath);
+    // 1. input -> directory path given
+    let destPath;
+    if (dirPath == undefined) {
+        destPath = process.cwd();
+        return;
+    } else {
+        let doesExist = fs.existsSync(dirPath);
+        if (doesExist) {
+
+            // 2. create -> organized_files -> directory
+            destPath = path.join(dirPath, "organized_files");
+            if (fs.existsSync(destPath) == false) {
+                fs.mkdirSync(destPath);
+            }
+
+        } else {
+
+            console.log("Kindly enter the correct path");
+            return;
+        }
+    }
+    organizeHelper(dirPath, destPath);
+    // 3. identify categories of all the files present in that input directory  ->
+}
+function organizeHelper(src, dest) {
+    // 3. identify categories of all the files present in that input directory  ->
+    let childNames = fs.readdirSync(src);
+    // console.log(childNames);
+    for (let i = 0; i < childNames.length; i++) {
+        let childAddress = path.join(src, childNames[i]);
+        let isFile = fs.lstatSync(childAddress).isFile();
+        if (isFile) {
+            // console.log(childNames[i]);
+            let category = getCategory(childNames[i]);
+            console.log(childNames[i], "belongs to --> ", category);
+            // 4. copy / cut  files to that organized directory inside of any of category folder 
+            sendFiles(childAddress, dest, category);
+        }
+    }
+}
+function sendFiles(srcFilePath, dest, category) {
+    // 
+    let categoryPath = path.join(dest, category);
+    if (fs.existsSync(categoryPath) == false) {
+        fs.mkdirSync(categoryPath);
+    }
+    let fileName = path.basename(srcFilePath);
+    let destFilePath = path.join(categoryPath, fileName);
+    fs.copyFileSync(srcFilePath, destFilePath);
+    fs.unlinkSync(srcFilePath);
+    console.log(fileName, "copied to ", category);
+
+}
+function getCategory(name) {
+    let ext = path.extname(name);
+    ext = ext.slice(1);
+    for (let type in types) {
+        let cTypeArray = types[type];
+        for (let i = 0; i < cTypeArray.length; i++) {
+            if (ext == cTypeArray[i]) {
+                return type;
+            }
+        }
+    }
+    return "others";
+}
+function treeFn(dirPath) {
+    // let destPath;
+    if (dirPath == undefined) {
+
+        treeHelper(process.cwd(), "");
+        return;
+    } else {
+        let doesExist = fs.existsSync(dirPath);
+        if (doesExist) {
+            treeHelper(dirPath, "");
+        } else {
+
+            console.log("Kindly enter the correct path");
+            return;
+        }
+    }
+}
+
+function treeHelper(dirPath, indent) {
+    // is file or folder
+    let isFile = fs.lstatSync(dirPath).isFile();
+    if (isFile == true) {
+        let fileName = path.basename(dirPath);
+        console.log(indent + "├──" + fileName);
+    } else {
+        let dirName = path.basename(dirPath)
+        console.log(indent + "└──" + dirName);
+        let childrens = fs.readdirSync(dirPath);
+        for (let i = 0; i < childrens.length; i++) {
+            let childPath = path.join(dirPath, childrens[i]);
+            treeHelper(childPath, indent + "\t");
+        }
+    }
 
 
 }
